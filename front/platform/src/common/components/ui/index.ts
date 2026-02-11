6e104bbaf08fe183e1b499af8d1336a1af8202c6
hongjeaho <teil83@kapanet.co.kr>
2026-02-11 18:17:33 +0900

    refactor(ui): UI 컴포넌트 export 경로 업데이트
    - 재구조화된 컴포넌트 경로에 맞춰 export 업데이트
    - action, form, data-display, layout 카테고리별 export 정리

--- front/platform/src/common/components/ui/index.ts
+++ front/platform/src/common/components/ui/index.ts
@@ -3,50 +3,67 @@
  * 디자인 시스템 기반 공통 컴포넌트 라이브러리
  */
 
-// Button
-export { Button } from './button/Button'
-export type { ButtonProps } from './button/Button.types'
+// Button (action)
+export { Button } from './action/button/Button'
+export type { ButtonProps } from './action/button/Button.types'
 
-// Checkbox
-export { Checkbox } from './checkbox/Checkbox'
-export type { CheckboxProps } from './checkbox/Checkbox.types'
+// Checkbox (form)
+export { Checkbox } from './form/checkbox/Checkbox'
+export type { CheckboxProps } from './form/checkbox/Checkbox.types'
 
-// FormCheckbox
-export { FormCheckbox } from './checkbox/FormCheckbox'
-export type { FormCheckboxProps } from './checkbox/FormCheckbox.types'
+// FormCheckbox (form)
+export { FormCheckbox } from './form/checkbox/FormCheckbox'
+export type { FormCheckboxProps } from './form/checkbox/FormCheckbox.types'
 
-// Input
-export { FormInput } from './input/FormInput'
-export type { FormInputProps } from './input/FormInput.types'
-export { Input } from './input/Input'
-export type { InputProps } from './input/Input.types'
+// Input (form)
+export { FormInput } from './form/input/FormInput'
+export type { FormInputProps } from './form/input/FormInput.types'
+export { Input } from './form/input/Input'
+export type { InputProps } from './form/input/Input.types'
 
-// Radio
-export type { RadioGroupProps, RadioOption } from './radio/Radio.types'
-export { RadioGroup } from './radio/RadioGroup'
+// Radio (form)
+export type { RadioGroupProps, RadioOption } from './form/radio/Radio.types'
+export { RadioGroup } from './form/radio/RadioGroup'
 
-// Select
-export { FormSelect } from './select/FormSelect'
-export type { FormSelectProps } from './select/FormSelect.types'
-export { Select } from './select/Select'
-export type { SelectOption, SelectProps } from './select/Select.types'
+// Select (form)
+export { FormSelect } from './form/select/FormSelect'
+export type { FormSelectProps } from './form/select/FormSelect.types'
+export { Select } from './form/select/Select'
+export type { SelectOption, SelectProps } from './form/select/Select.types'
 
-// Upload
-export { Upload } from './upload/Upload'
-export type { UploadMultiProps, UploadProps } from './upload/Upload.types'
-export { UploadMulti } from './upload/UploadMulti'
+// Upload (form)
+export { Upload } from './form/upload/Upload'
+export type { UploadMultiProps, UploadProps } from './form/upload/Upload.types'
+export { UploadMulti } from './form/upload/UploadMulti'
 
-// Table
-export { Table } from './table/Table'
-export type { TableColumn, TableProps, TableSortConfig } from './table/Table.types'
+// DatePicker (form)
+export { DatePicker } from './form/datePicker/DatePicker'
+export type { DatePickerProps } from './form/datePicker/DatePicker.types'
 
-// DatePicker
-export { DatePicker } from '@/common/components/ui/datePicker/DatePicker'
-export type { DatePickerProps } from '@/common/components/ui/datePicker/DatePicker.types'
+// DateRangePicker (form)
+export { DateRangePicker } from './form/dateRangePicker/DateRangePicker'
+export type { DateRange, DateRangePickerProps } from './form/dateRangePicker/DateRangePicker.types'
 
-// DateRangePicker
-export { DateRangePicker } from '@/common/components/ui/dateRangePicker/DateRangePicker.tsx'
+// Table (data-display)
+export { Table } from './data-display/table/Table'
+export type { TableColumn, TableProps, TableSortConfig } from './data-display/table/Table.types'
+
+// Card (layout)
+export {
+  Card,
+  CardHeader,
+  CardContent,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+} from './layout/card/Card'
 export type {
-  DateRange,
-  DateRangePickerProps,
-} from '@/common/components/ui/dateRangePicker/DateRangePicker.types.ts'
+  CardProps,
+  CardSize,
+  CardVariant,
+  CardHeaderProps,
+  CardContentProps,
+  CardFooterProps,
+  CardTitleProps,
+  CardDescriptionProps,
+} from './layout/card/Card.types'
